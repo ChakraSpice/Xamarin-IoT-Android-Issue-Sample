4544f7fc2ef384bd7e325d90479c579a4d92bc49
Damir Bajramovic <bajramovic.damir@hotmail.com>
2018-03-30 10:27:57 +0200

    Add project files.

---
+++ IoTX/IoTX/IoT.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTX
+{ 
+    public class IoT
+    {
+        private DeviceClient deviceClient;
+
+        public IoT(string connectionString)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Http1);
+        }
+
+        public async Task SendMessage(object data)
+        {
+            try
+            {
+                Message message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)));
+                if (deviceClient != null)
+                {
+                    await deviceClient.SendEventAsync(message);
+                }
+                else Debug.WriteLine("Message failed to send" + JsonConvert.SerializeObject(data));
+            }
+            catch
+            {
+                Debug.WriteLine("EXCEPTION!");
+            }
+        }
+    }
+}
---
+++ IoTX/IoTX/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IoTX
+{
+	public partial class MainPage : ContentPage
+	{
+        private string connectionString;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void ClickedHandler(object sender, EventArgs e)
+        {
+            IoT instance = new IoT(connectionString);
+            await instance.SendMessage("HEHE IT WORKS!");
+        }
+
+    }
+}
